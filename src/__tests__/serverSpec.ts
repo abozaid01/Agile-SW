87cb1b3f99ad3ff032a5706d6ca06e23e4af9a89
Mohamed Abozaid <mohamed.abozaid01@gmail.com>
2023-03-21 18:26:56 +0200

    add unit test for db connection

--- src/__tests__/serverSpec.ts
+++ src/__tests__/serverSpec.ts
@@ -1,12 +1,40 @@
 import supertest from 'supertest';
 import app from '../server';
+import config from '../config';
 
-//create a request object
 const request = supertest(app);
 
 describe('test basic endpoint', () => {
     it('Get the endpoint /', async () => {
         const response = await request.get('/');
         expect(response.status).toBe(200);
+    });
+});
+
+import { Pool } from 'pg';
+
+// Describe the test suite
+describe('Postgres Pool Connection Test', () => {
+    let pool: Pool;
+
+    beforeAll(() => {
+        pool = new Pool({
+            user: config.user,
+            host: config.host,
+            database: config.databse,
+            password: config.password,
+            port: parseInt(config.dbPort as string, 10),
+        });
+    });
+
+    afterAll(async () => {
+        await pool.end();
+    });
+
+    it('should connect to the Postgres database', async () => {
+        const client = await pool.connect();
+        const res = await client.query('SELECT NOW()');
+        expect(res.rows[0].now instanceof Date).toBe(true);
+        client.release();
     });
 });
