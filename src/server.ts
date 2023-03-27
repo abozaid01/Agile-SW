db7b2468fb95fa5e2b6f4db5470af4e3ec675372
Mohamed Abozaid <mohamed.abozaid01@gmail.com>
2023-03-27 22:53:30 +0200

    fix login issue

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,7 @@ const app: Application = express();
 //middlewares
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/view'));
